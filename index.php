746e6d98c9fa5bf8ce474072fb43f4201e27b0bd
Imikino <jeanluc05@live.com>
2017-09-05 11:04:54 +0200

    jjskjsk

--- index.php
+++ index.php
@@ -47,7 +47,16 @@
 </head>
 
 <body>
-<script type="text/javascript" src="//go.pub2srv.com/apu.php?zoneid=1378796"></script>
+<script>
+(function(i,s,o,g,r,­a,m){i['GoogleAnalyt­icsObject']=r;i[r]=i­[r]||function(){
+(i[r].q=i[r].q||[]).­push(arguments)},i[r­].l=1*new Date();a=s.createEle­ment(o),
+m=s.getElementsByTag­Name(o)[0];a.async=1­;a.src=g;m.parentNod­e.insertBefore(a,m)
+})(window,document,'­script','https://­www.google-analytics.­com/analytics.js','ga');
+
+ga('create', 'UA-102050556-1', 'auto');
+ga('send', 'pageview');
+
+</script>
 <script>
   window.fbAsyncInit = function() {
     FB.init({
